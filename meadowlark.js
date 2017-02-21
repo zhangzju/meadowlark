29a273c8fbfa04b2186e16b640c8ed1485854de9
zhangzju <zhangway19921221@163.com>
2017-02-21 11:07:18 +0800

    Initial commit

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,24 @@
-var express = reuqire('express');
+var express = require('express');
 
-var app = exspress();
+var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-//定制404页面
+//定制404页面：not found
 app.use(function(req,res){
     res.type('text/plain');
     res.status(404);
     res.send('404 - Not Found');
-})
+});
+
+//定制500页面：internal server error
+app.use(function(err, req, res,next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Meadowlark started on http://localhost'+app.get('port')+'!');
+});
