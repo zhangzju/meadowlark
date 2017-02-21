ec596bda6062960d85f1b64f58e126a20873e6ea
zhangzju <zhangway19921221@163.com>
2017-02-21 18:00:38 +0800

    add layout

--- meadowlark.js
+++ meadowlark.js
@@ -1,22 +1,34 @@
 var express = require('express');
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 
 var app = express();
 
+//配置模板引擎
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+});
 
 //定制404页面：not found
 app.use(function(req,res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 //定制500页面：internal server error
 app.use(function(err, req, res,next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
